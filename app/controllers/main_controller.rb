c8ef723d91f55cc500f60cb6fb1b7e68c9dca252
Simon Lee <simonhlee97@hotmail.com>
2022-07-01 15:22:12 +0900

    can access the current user

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,8 +2,5 @@ class MainController < ApplicationController
   def index
     # flash[:notice] = "You are logged in"
     # flash[:alert] = "Invalid email or password"
-    if session[:user_id]
-      @user = User.find_by(id: session[:user_id])
-    end
   end
 end
---
+++ app/models/current.rb
@@ -0,0 +1,5 @@
+# current User Model
+class Current < ActiveSupport::CurrentAttributes
+  attribute :user
+
+end
